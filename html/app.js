93b5aef0ee46b05bebd9dc61d3629e94bfd5a84d
pskrabneu <skrabneu@gmail.com>
2016-09-20 19:25:09 +0300

    Chapter 2. Default parameters.

--- html/app.js
+++ html/app.js
@@ -1,4 +1,15 @@
 /**
  * Created by pskrebnev on 19.09.2016.
  */
+var container = document.getElementById('container');
+function countdown(initial, final, interval) {
+    if (final === void 0) { final = 0; }
+    if (interval === void 0) { interval = 1; }
+    var current = initial;
+    while (current > final) {
+        container.innerHTML = current;
+        current -= interval;
+    }
+}
+countdown(10, 0, 1);
 //# sourceMappingURL=app.js.map
--- html/app.ts
+++ html/app.ts
@@ -2,6 +2,22 @@
  * Created by pskrebnev on 19.09.2016.
  */
 
+var container = document.getElementById('container');
+
+function countdown(initial, final = 0, interval = 1) {
+
+    var current = initial;
+
+    while (current > final) {
+        container.innerHTML = current;
+        current -= interval;
+    }
+}
+
+countdown(10, 0, 1);
+
+
+
 
 
 
