d34082114dac450436c0ede0fe8589547d631970
pskrabneu <skrabneu@gmail.com>
2016-09-21 19:01:08 +0300

    Chapter 2. Video 3. Tricks with default parameters, conditions, templates

--- html/app.js
+++ html/app.js
@@ -2,14 +2,22 @@
  * Created by pskrebnev on 19.09.2016.
  */
 var container = document.getElementById('container');
-function countdown(initial, final, interval) {
-    if (final === void 0) { final = 0; }
-    if (interval === void 0) { interval = 1; }
-    var current = initial;
-    while (current > final) {
-        container.innerHTML = current;
-        current -= interval;
-    }
-}
-countdown(10, 0, 1);
+// function countdown(initial, final = 0, interval = 1) {
+//
+//     var current = initial;
+//
+//     while (current > final) {
+//         container.innerHTML = current;
+//         current -= interval;
+//     }
+// }
+//
+// countdown(10, 0, 1);
+var todo = {
+    id: 123,
+    name: "Pick up drycleaning",
+    completed: true
+};
+// var displayName = `Todo #${todo.id}`
+container.innerHTML = "\n<div todo='" + todo.id + "' class=\"list-group-item}\">\n    <i class=\"" + (todo.completed ? "" : "hidden") + " text-success glyphicon glyphicon-ok\"></i>\n    <span class=\"name\">" + todo.name + "</span>\n</div>\n";
 //# sourceMappingURL=app.js.map
--- html/app.ts
+++ html/app.ts
@@ -4,20 +4,32 @@
 
 var container = document.getElementById('container');
 
-function countdown(initial, final = 0, interval = 1) {
+// function countdown(initial, final = 0, interval = 1) {
+//
+//     var current = initial;
+//
+//     while (current > final) {
+//         container.innerHTML = current;
+//         current -= interval;
+//     }
+// }
+//
+// countdown(10, 0, 1);
 
-    var current = initial;
-
-    while (current > final) {
-        container.innerHTML = current;
-        current -= interval;
-    }
+var todo = {
+    id: 123,
+    name: "Pick up drycleaning",
+    completed: true
 }
 
-countdown(10, 0, 1);
+// var displayName = `Todo #${todo.id}`
 
-
-
+container.innerHTML = `
+<div todo='${todo.id}' class="list-group-item}">
+    <i class="${ todo.completed ? "": "hidden"} text-success glyphicon glyphicon-ok"></i>
+    <span class="name">${todo.name}</span>
+</div>
+`
 
 
 
