5b394198b49ae61e773f387515dd9ceb9c65ba14
pskrabneu <skrabneu@gmail.com>
2016-09-26 19:59:38 +0300

    Chapter 2. Video 06. Lambda Expressions.

--- html/app.js
+++ html/app.js
@@ -1,17 +1,30 @@
 /**
  * Created by pskrebnev on 19.09.2016.
  */
-var array = [
+/*var array = [
     "Pick up drycleaning",
     "Clean Batcave",
     "Save Gotham"
 ];
+
 for (var index in array) {
     var strValue = array[index];
-    console.log(index + ": " + strValue);
+    console.log(`${index}: ${strValue}`);
 }
-for (var _i = 0, array_1 = array; _i < array_1.length; _i++) {
-    var value = array_1[_i];
-    console.log("== " + value + " ==");
+
+for (var value of array) {
+    console.log(`== ${value} ==`);
+}*/
+//Video 06-Lambdas
+var container = document.getElementById('container');
+function Counter(el) {
+    var _this = this;
+    this.count = 0;
+    el.innerHTML = this.count;
+    el.addEventListener('click', function () {
+        _this.count += 1;
+        el.innerHTML = _this.count;
+    });
 }
+new Counter(container);
 //# sourceMappingURL=app.js.map
--- html/app.ts
+++ html/app.ts
@@ -2,7 +2,8 @@
  * Created by pskrebnev on 19.09.2016.
  */
 
-var array = [
+
+/*var array = [
     "Pick up drycleaning",
     "Clean Batcave",
     "Save Gotham"
@@ -15,7 +16,27 @@ for (var index in array) {
 
 for (var value of array) {
     console.log(`== ${value} ==`);
+}*/
+
+//Video 06-Lambdas
+var container = document.getElementById('container');
+
+function Counter(el) {
+    this.count = 0;
+
+    el.innerHTML = this.count;
+
+    el.addEventListener('click',
+    () => {
+        this.count += 1;
+        el.innerHTML = this.count;
+    })
 }
+
+new Counter(container);
+
+
+
 
 
 
