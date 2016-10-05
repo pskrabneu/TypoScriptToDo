db0f46a1c6945d1ba69f54c1019a3d472d88dc85
pskrabneu <skrabneu@gmail.com>
2016-10-05 19:53:08 +0300

    Chapter 2. Video 09. Computed properties.

--- html/app.js
+++ html/app.js
@@ -3,33 +3,48 @@
  */
 // Chapter 02 > 02-08
 // The spread operator
-function calculate(action) {
-    var values = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        values[_i - 1] = arguments[_i];
-    }
+/*function calculate(action, ...values) {
     var total = 0;
-    for (var _a = 0, values_1 = values; _a < values_1.length; _a++) {
-        var value = values_1[_a];
+
+    for (var value of values) {
+
         switch (action) {
             case 'add':
                 total += value;
                 break;
+
             case 'substract':
                 total -= value;
                 break;
         }
     }
+
     return total;
 }
-console.log(calculate('add', 1, 2, 3, 4, 5));
+
+console.log(calculate('add', 1, 2, 3, 4, 5));*/
 // Spreading array
-var sourse = [5, 6, 7];
-var target = [1, 2, 3, 4].concat(sourse, [8, 9]);
-console.log(target);
+/*var sourse = [5, 6, 7];
+var target = [1, 2, 3, 4, ...sourse, 8, 9];
+console.log(target);*/
 // Spreading array
-var list1 = [1, 3, 4, 5];
-var list2 = [6, 7, 8];
-list1.push.apply(list1, list2);
-console.log(list1);
+/*var list1 = [1,3,4,5];
+var list2 = [6,7,8];
+
+list1.push(...list2);
+console.log(list1);*/
+// Chapter 02 > 02-09
+// Computed properties
+var osPrefix = 'os_';
+var support = (_a = {},
+    _a[osPrefix + 'Windows'] = isSupported('Windows'),
+    _a[osPrefix + 'iOS'] = isSupported('iOS'),
+    _a[osPrefix + 'Android'] = isSupported('Android'),
+    _a
+);
+function isSupported(os) {
+    return Math.random() >= 0.5;
+}
+console.log(support);
+var _a;
 //# sourceMappingURL=app.js.map
--- html/app.ts
+++ html/app.ts
@@ -4,7 +4,7 @@
 
 // Chapter 02 > 02-08
 // The spread operator
-function calculate(action, ...values) {
+/*function calculate(action, ...values) {
     var total = 0;
 
     for (var value of values) {
@@ -23,18 +23,37 @@ function calculate(action, ...values) {
     return total;
 }
 
-console.log(calculate('add', 1, 2, 3, 4, 5));
+console.log(calculate('add', 1, 2, 3, 4, 5));*/
 
 // Spreading array
-var sourse = [5, 6, 7];
+/*var sourse = [5, 6, 7];
 var target = [1, 2, 3, 4, ...sourse, 8, 9];
-
-console.log(target);
+console.log(target);*/
 
 // Spreading array
-var list1 = [1,3,4,5];
+/*var list1 = [1,3,4,5];
 var list2 = [6,7,8];
 
 list1.push(...list2);
-console.log(list1);
+console.log(list1);*/
+
+
+// Chapter 02 > 02-09
+// Computed properties
+const osPrefix = 'os_';
+
+var support = {
+    [osPrefix + 'Windows']: isSupported('Windows'),
+    [osPrefix + 'iOS']: isSupported('iOS'),
+    [osPrefix + 'Android']: isSupported('Android')
+}
+
+function isSupported(os) {
+    return Math.random() >= 0.5;
+}
+
+console.log(support);
+
+
+
 
