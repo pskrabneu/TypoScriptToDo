265b392fdd85e1193734a6da8159e9a4e7bcef44
pskrabneu <skrabneu@gmail.com>
2016-10-06 19:29:11 +0300

    Chapter 3. Video 01. JavaScript types.

--- html/app.js
+++ html/app.js
@@ -4,47 +4,61 @@
 // Chapter 02 > 02-08
 // The spread operator
 /*function calculate(action, ...values) {
-    var total = 0;
+ var total = 0;
 
-    for (var value of values) {
+ for (var value of values) {
 
-        switch (action) {
-            case 'add':
-                total += value;
-                break;
+ switch (action) {
+ case 'add':
+ total += value;
+ break;
 
-            case 'substract':
-                total -= value;
-                break;
-        }
-    }
+ case 'substract':
+ total -= value;
+ break;
+ }
+ }
 
-    return total;
-}
+ return total;
+ }
 
-console.log(calculate('add', 1, 2, 3, 4, 5));*/
+ console.log(calculate('add', 1, 2, 3, 4, 5));*/
 // Spreading array
 /*var sourse = [5, 6, 7];
-var target = [1, 2, 3, 4, ...sourse, 8, 9];
-console.log(target);*/
+ var target = [1, 2, 3, 4, ...sourse, 8, 9];
+ console.log(target);*/
 // Spreading array
 /*var list1 = [1,3,4,5];
-var list2 = [6,7,8];
+ var list2 = [6,7,8];
 
-list1.push(...list2);
-console.log(list1);*/
+ list1.push(...list2);
+ console.log(list1);*/
 // Chapter 02 > 02-09
 // Computed properties
-var osPrefix = 'os_';
-var support = (_a = {},
-    _a[osPrefix + 'Windows'] = isSupported('Windows'),
-    _a[osPrefix + 'iOS'] = isSupported('iOS'),
-    _a[osPrefix + 'Android'] = isSupported('Android'),
-    _a
-);
-function isSupported(os) {
-    return Math.random() >= 0.5;
+/*const osPrefix = 'os_';
+
+ var support = {
+ [osPrefix + 'Windows']: isSupported('Windows'),
+ [osPrefix + 'iOS']: isSupported('iOS'),
+ [osPrefix + 'Android']: isSupported('Android')
+ }
+
+ function isSupported(os) {
+ return Math.random() >= 0.5;
+ }
+
+ console.log(support);*/
+// Chapter 3 > 03-01
+// JavaScript types
+var animal = {
+    name: "Fido",
+    species: "Dog",
+    age: 5,
+    speak: function () {
+        console.log('Woof! Woof!');
+    }
+};
+function makeAnimalSpeak(animal) {
+    animal.speak();
 }
-console.log(support);
-var _a;
 //# sourceMappingURL=app.js.map
--- html/app.ts
+++ html/app.ts
@@ -5,55 +5,68 @@
 // Chapter 02 > 02-08
 // The spread operator
 /*function calculate(action, ...values) {
-    var total = 0;
+ var total = 0;
 
-    for (var value of values) {
+ for (var value of values) {
 
-        switch (action) {
-            case 'add':
-                total += value;
-                break;
+ switch (action) {
+ case 'add':
+ total += value;
+ break;
 
-            case 'substract':
-                total -= value;
-                break;
-        }
-    }
+ case 'substract':
+ total -= value;
+ break;
+ }
+ }
 
-    return total;
-}
+ return total;
+ }
 
-console.log(calculate('add', 1, 2, 3, 4, 5));*/
+ console.log(calculate('add', 1, 2, 3, 4, 5));*/
 
 // Spreading array
 /*var sourse = [5, 6, 7];
-var target = [1, 2, 3, 4, ...sourse, 8, 9];
-console.log(target);*/
+ var target = [1, 2, 3, 4, ...sourse, 8, 9];
+ console.log(target);*/
 
 // Spreading array
 /*var list1 = [1,3,4,5];
-var list2 = [6,7,8];
+ var list2 = [6,7,8];
 
-list1.push(...list2);
-console.log(list1);*/
+ list1.push(...list2);
+ console.log(list1);*/
 
 
 // Chapter 02 > 02-09
 // Computed properties
-const osPrefix = 'os_';
+/*const osPrefix = 'os_';
 
-var support = {
-    [osPrefix + 'Windows']: isSupported('Windows'),
-    [osPrefix + 'iOS']: isSupported('iOS'),
-    [osPrefix + 'Android']: isSupported('Android')
+ var support = {
+ [osPrefix + 'Windows']: isSupported('Windows'),
+ [osPrefix + 'iOS']: isSupported('iOS'),
+ [osPrefix + 'Android']: isSupported('Android')
+ }
+
+ function isSupported(os) {
+ return Math.random() >= 0.5;
+ }
+
+ console.log(support);*/
+
+// Chapter 3 > 03-01
+// JavaScript types
+var animal = {
+    name: "Fido",
+    species: "Dog",
+    age: 5,
+    speak: function () {
+        console.log('Woof! Woof!');
+    }
 }
 
-function isSupported(os) {
-    return Math.random() >= 0.5;
+function makeAnimalSpeak(animal) {
+    animal.speak();
 }
-
-console.log(support);
-
-
 
 
