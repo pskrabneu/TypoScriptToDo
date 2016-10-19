e774ea2dd3667ed6c6abc3f4938f04299adf9094
pskrabneu <skrabneu@gmail.com>
2016-10-19 19:07:36 +0300

    Chapter 6. Video 01. Generics. Part 1.

--- html/app.js
+++ html/app.js
@@ -1,6 +1,18 @@
 /**
  * Created by pskrebnev on 19.09.2016.
  */
-// Chapter 5 "" >
-// Video08 "Implementing interfaces".
+// Chapter 6 "Generics" >
+// Video01 "Introducing generics".
+function clone(value) {
+    var serialized = JSON.stringify(value);
+    return JSON.parse(serialized);
+}
+var test1 = {
+    id: 1,
+    name: "Pick up my car",
+    state: false
+};
+console.log(clone(125));
+console.log(clone("double"));
+console.log(clone(test1));
 //# sourceMappingURL=app.js.map
--- html/app.ts
+++ html/app.ts
@@ -2,8 +2,26 @@
  * Created by pskrebnev on 19.09.2016.
  */
 
-// Chapter 5 "" >
-// Video08 "Implementing interfaces".
+// Chapter 6 "Generics" >
+// Video01 "Introducing generics".
+
+function clone<T>(value: T): T {
+    let serialized = JSON.stringify(value);
+    return JSON.parse(serialized);
+}
+
+var test1: any = {
+    id: 1,
+    name: "Pick up my car",
+    state: false
+};
+
+console.log(clone(125));
+console.log(clone("double"));
+console.log(clone(test1));
+
+
+
 
 
 
